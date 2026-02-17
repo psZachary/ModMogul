1a245ba04e6285d9301531d4c13212550afd173f
Zachary <47261750+psZachary@users.noreply.github.com>
2026-02-17 14:21:28 -0800

    Delete obj directory

--- obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ModMogul
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ModMogul";
-        public const string PLUGIN_NAME = "ModMogul";
-        public const string PLUGIN_VERSION = "0.3.1";
-    }
-}
